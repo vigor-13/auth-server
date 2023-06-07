91112608b7a7f30af126699828261392d714420d
vigor-13 <vigor13.dev@gmail.com>
2023-06-08 02:29:06 +0900

    build: add @nestjs/swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './modules/app';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const appConfig = app.get(ConfigService).get('config');
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Auth Server Example')
+    .setVersion('1.0.0')
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+
+  SwaggerModule.setup('api', app, swaggerDocument);
 
   await app.listen(appConfig.http.port, () => {
     console.log(
