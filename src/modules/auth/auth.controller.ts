2aa0e41c24a6eb51f4a6cd4cb96bcd7f65e3c4d5
vigor-13 <vigor13.dev@gmail.com>
2023-06-09 23:06:33 +0900

    feat: add signIn mockup api

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Post('signIn')
+  signIn(@Body() props: Record<string, any>) {
+    return this.authService.signIn({
+      email: props.email,
+      password: props.password,
+    });
+  }
+}
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UserService } from '../user/user.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private userService: UserService) {}
+
+  async signIn(props: { email: string; password: string }): Promise<any> {
+    const { email, password } = props;
+
+    const user = await this.userService.findUser({
+      email,
+    });
+
+    if (user?.password !== password) {
+      throw new UnauthorizedException();
+    }
+
+    delete user.password;
+
+    return user;
+  }
+}
